22d7f96732043bdc63ff92c07c87a0561e6d161a
MacKaiver <mackaiver@freenet.de>
2014-03-17 17:52:41 +0100

    First somewhat functioning version. Fucking css horror

--- app/view.py
+++ app/view.py
@@ -3,6 +3,7 @@ __author__ = 'mackaiver'
 from flask import render_template
 from seins.PageParser import DBPageParser, PageContentError
 from seins.HtmlFetcher import FetcherException
+import json
 from app import app
 
 
@@ -17,12 +18,19 @@ def index():
 @app.route('/connections/')
 def connections():
 
-
     try:
-        page = DBPageParser(departing_station, arrival_station)
+        page = DBPageParser("Universität Dortmund S-Bahnhof", "Dortmund Hbf")
+        traintuples = page.connections
+        trains = list()
+        for t in traintuples:
+            trains.append({"departure_time": t[0], "arrival_time": t[1], "delay": t[2], "connection_type": t[3]})
+        jsonObj = json.dumps(trains)
+        return (jsonObj)
 
     except PageContentError as e:
         print('Webpage returned an error message: ' + str(e))
+        return "Error page content"
 
     except FetcherException as e:
         print('Fetcher could not get valid response from server: ' + str(e))
+        return "Error fetcher"
