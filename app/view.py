4e10b3b5022652e4a7200e2e47a414c83c92cb76
MacKaiver <mackaiver@freenet.de>
2014-03-17 16:02:29 +0100

    First quick design.

--- app/view.py
+++ app/view.py
@@ -1,9 +1,28 @@
 __author__ = 'mackaiver'
 
+from flask import render_template
+from seins.PageParser import DBPageParser, PageContentError
+from seins.HtmlFetcher import FetcherException
 from app import app
+
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    text = "Hello biotches with teh bitch bioprodukte!"
+    return render_template("main.html", delaystring='Pünktlich', text=text)
+
+
+@app.route('/connections/')
+def connections():
+
+
+    try:
+        page = DBPageParser(departing_station, arrival_station)
+
+    except PageContentError as e:
+        print('Webpage returned an error message: ' + str(e))
+
+    except FetcherException as e:
+        print('Fetcher could not get valid response from server: ' + str(e))
