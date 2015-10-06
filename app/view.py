b6cf7bb3c5e92b309106b87554b774d22ab46c3c
Kai <kai.bruegge@gmail.com>
2015-10-06 21:08:50 +0200

    added the ontime route and set departuretime back 5 minutes

--- app/view.py
+++ app/view.py
@@ -6,6 +6,7 @@ from seins.PageParser import DBPageParser, PageContentError
 from seins.HtmlFetcher import FetcherException
 import json
 import time
+import datetime
 from app import app
 
 
@@ -14,18 +15,52 @@ from app import app
 def index():
     return render_template("main.html")
 
+##no if delayed. yes if on time
+@app.route('/ontime/')
+def  yesno():
+    try:
+        departure_time = (datetime.datetime.now() - datetime.timedelta(minutes=5)).strftime("%H:%M")
+        page = DBPageParser("Dortmund Universität", "Dortmund Hbf", departure_time=departure_time)
+        traintuples = page.connections
+        delay = None
+        for t in traintuples:
+            print(t)
+            if t[3] and str(t[3]).lower().strip() == 's':
+                delay =  t[2]
+        if delay is None:
+            return "maybe"
+        try:
+            delay = int(delay)
+            if delay < 4:
+                return "yes"
+            else:
+                return "no"
+        except ValueError as e:
+                return "no"
+
+    except PageContentError as e:
+        print('Webpage returned an error message: ' + str(e))
+        return json.dumps("error")
+
+    except FetcherException as e:
+        print('Fetcher could not get valid response from server: ' + str(e))
+        return json.dumps("error")
+
+
 
 @app.route('/connections/')
 def connections():
     #print(direction.lower())
     direction = request.args.get('direction', '')
-    time.sleep(3)
+    # time.sleep(3)
+    departure_time = (datetime.datetime.now() - datetime.timedelta(minutes=5)).strftime("%H:%M")
+
     try:
         if direction and direction.lower().strip() == "solingen":
             #print("Requesting solingen")
-            page = DBPageParser("Dortmund Universität", "Solingen Hbf")
+            page = DBPageParser("Dortmund Universität", "Solingen Hbf", departure_time=departure_time)
         else:
-            page = DBPageParser("Dortmund Universität", "Dortmund Hbf")
+            page = DBPageParser("Dortmund Universität", "Dortmund Hbf", departure_time=departure_time)
 
         traintuples = page.connections
         trains = list()
